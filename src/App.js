09e9ad282f093d4bc234458a0216e5f1144f590a
Lucas Gonçalves <ldgoncalvs@gmail.com>
2020-06-28 16:01:15 -0300

    Updating react app with a new components and shop page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import {Switch, Route } from 'react-router-dom'
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 import './App.css';
 
 
-const HatsPage = () => (
-  <div>
-    <h1>HatsPage</h1>
-  </div>
-);
+// const HatsPage = () => (
+//   <div>
+//     <h1>HatsPage</h1>
+//   </div>
+// );
 
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
     <div>
 
       <Route exact path='/' component={HomePage}/>
-      <Route exact path='/hats' component={HatsPage}/>
+      <Route exact path='/shop' component={ShopPage}/>
 
     
     </div>
