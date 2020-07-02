ac5ae2e009481c68bf440f0fb74fa6a591861573
Lucas Gonçalves <ldgoncalvs@gmail.com>
2020-07-02 14:38:53 -0300

    Adding Navbar and links

--- src/App.js
+++ src/App.js
@@ -2,22 +2,21 @@ import React from 'react';
 import {Switch, Route } from 'react-router-dom'
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from './components/header/header.component';
 import './App.css';
 
 
-// const HatsPage = () => (
-//   <div>
-//     <h1>HatsPage</h1>
-//   </div>
-// );
 
 
 function App() {
   return (
-    <div>
-
-      <Route exact path='/' component={HomePage}/>
+    <div> 
+    <Header/>
+    <Switch>
+    <Route exact path='/' component={HomePage}/>
       <Route exact path='/shop' component={ShopPage}/>
+    </Switch>
+     
 
     
     </div>
