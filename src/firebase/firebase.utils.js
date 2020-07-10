28ff98a897418885bd3d77fda4b351da82fcff3d
Lucas Gonçalves <ldgoncalvs@gmail.com>
2020-07-09 23:59:51 -0300

    setting up firebase in our app

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,32 @@ const config = {
     appId: "1:539010757209:web:979a9408c60745036c7748"
   };
 
+export const createUserProfileDocument = async (userAuth, additionalData) =>{
+  if(!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`)
+   const snapShot = await userRef.get();
+
+  if(!snapShot.exists){
+    const { displayName, email} = userAuth;
+    const createdAt = new Date();
+
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+        console.log('error creating user', error.message)
+    }
+  }
+
+  return userRef;
+}
+
 
 firebase.initializeApp(config);
 
