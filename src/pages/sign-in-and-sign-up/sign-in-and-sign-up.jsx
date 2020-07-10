a0689485d89c1d343079a8497909f11681899345
Lucas Gonçalves <ldgoncalvs@gmail.com>
2020-07-10 00:55:30 -0300

    Now we are able to store users in our app

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import SignIn from '../../components/sign-in/signIn.component';
 import './sign-in-and-sign-up.scss';
 
+import SignUp from '../../components/sign-up/sign-up.component';
+
 
 const SignInAndSignUpPage= () => (
 
     <div className='sign-in-and-sign-up'>
         <SignIn></SignIn>
+        <SignUp></SignUp>
     </div>
 
 )
