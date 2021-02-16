dcf2677ab92e760a5ce132df4bed216e91559e14
Lucas Gonçalves <ldgoncalvs@gmail.com>
2021-02-16 19:48:52 -0300

    Adding Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import {Route } from 'react-router'
+import { BrowserRouter as Router } from 'react-router-dom'
 import Home from './pages/home.component'
+import ShopPage from './pages/shop/shop.component';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <div>
-          <Home/>
+          <Router>
+  
+            <Route exact path='/' component={Home}/>
+            <Route exact path='/hats' component={ShopPage}/>
+
+          </Router>
+   
         </div>
     </div>
   );
