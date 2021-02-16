f315a6c0c31718b98d6265eed69df5336190a850
Lucas Gonçalves <ldgoncalvs@gmail.com>
2021-02-16 18:20:44 -0300

    ReAdding Home Component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Home from './components/home.component'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div>
+          <Home/>
+        </div>
+    </div>
+  );
+}
+
+export default App;
