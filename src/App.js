482910671b7cfc41b831399b5a9d0cc94a4108e8
Lucas Gonçalves <ldgoncalvs@gmail.com>
2021-02-19 06:58:15 -0300

    Adding Header Component and Routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import {Route } from 'react-router'
 import { BrowserRouter as Router } from 'react-router-dom'
+
+import './App.css';
+
 import Home from './pages/home.component'
 import ShopPage from './pages/shop/shop.component';
-import './App.css';
+import Header from './components/header/header.component'
 
 function App() {
   return (
     <div className="App">
         <div>
+   
           <Router>
-  
+          <Header/>
             <Route exact path='/' component={Home}/>
             <Route exact path='/shop' component={ShopPage}/>
 
