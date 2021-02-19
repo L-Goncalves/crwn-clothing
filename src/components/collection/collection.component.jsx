b7eeaea67e4c5192d9c63503115208cbb53690f4
Lucas Gonçalves <ldgoncalvs@gmail.com>
2021-02-19 00:06:21 -0300

    adding collections components

--- src/components/collection/collection.component.jsx
+++ src/components/collection/collection.component.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import './collection.style.scss';
+import CollectionItem from './colletion-item.component'
+
+
 const CollectionPreview = ({title, items}) =>{
     return(
 
     <div className='collection-preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
-        {items.filter((item, idx) => idx < 4).map(item =>{
-            return <div key={item.id}>{item.name}</div>
+        {items
+            .filter((item, idx) => idx < 4)
+            .map( ({id, ...other}) =>{
+            return (
+            <CollectionItem key={id}  {...other}/>
+            
+            )
         })}
         </div>
     </div>
