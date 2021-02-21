2288330878799270c5b3ca2ecdb48ee2730135c8
l-goncalves <ldgoncalvs@gmail.com>
2021-02-21 18:33:25 -0300

    Adding signOut Component

--- src/pages/signIn-signUp/sign-in-sign-up.component.jsx
+++ src/pages/signIn-signUp/sign-in-sign-up.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import SignIn from '../../components/signIn/signIn.component'
-
+import SignOut from '../../components/signOut/sign-out.component'
 const signInSignUp = () =>{
     return(
         <div>
             <SignIn/>
+            <SignOut/>
         </div>
 
     )
